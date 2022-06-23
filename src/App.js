1fccd3ca8f1ed9b9b35fb409691a9f281e852f54
tuannt2000 <79615528+tuannt2000@users.noreply.github.com>
2022-06-23 14:28:18 +0700

    use firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 /* スタイルシート */
 import './styles/main.css';
 
---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1UpYmkCNIMZZ7OugsD84L2F0AJ90SH_8",
+  authDomain: "fir-sample-f77a2.firebaseapp.com",
+  projectId: "fir-sample-f77a2",
+  storageBucket: "fir-sample-f77a2.appspot.com",
+  messagingSenderId: "1086923384613",
+  appId: "1:1086923384613:web:2e7c4da3f693d6a6e41ccc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
